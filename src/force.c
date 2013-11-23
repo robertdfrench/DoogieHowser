c912f2f69dce5cb6ce01a49b3d339b1186ca860a
Robert D. French <robert@robertdfrench.me>
2013-11-23 01:04:23 -0500

    Add force calculation

---
+++ src/force.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <math.h>
+#include "force.h"
+
+double force_calculate_magnitude(double x1, double y1, double x2, double y2) {
+	   return sqrt(pow(x1 - x2,2) + pow(y1 - y2,2));
+}
--- tests/check_force.c
+++ tests/check_force.c
@@ -1,3 +1,30 @@
+#include <stdlib.h>
+#include <check.h>
+#include "../src/force.h"
+
+START_TEST (test_force_is_zero_for_same_particle)
+{
+  double x1,y1 = 0.1;
+  int force = (int) force_calculate_magnitude(x1,y1,x1,y1);
+  ck_assert_int_eq(force, 0);
+}
+END_TEST
+
+Suite* force_suite(void) {
+	   Suite *s = suite_create("Force");
+	   TCase *tc_core = tcase_create("Core");
+	   tcase_add_test(tc_core,test_force_is_zero_for_same_particle);
+	   suite_add_tcase(s, tc_core);
+
+	   return s;
+}
+
 int main (void) {
-  return 0;
+	   int number_failed;
+	   Suite *s = force_suite();
+	   SRunner *sr = srunner_create(s);
+	   srunner_run_all(sr, CK_NORMAL);
+	   number_failed = srunner_ntests_failed (sr);
+	   srunner_free (sr);
+	   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }

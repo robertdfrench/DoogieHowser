3918dacaeffddfef5615683293021c32b0a1a2da
Robert D. French <frenchrd@ornl.gov>
2015-01-02 10:29:56 -0500

    Throw everything out

--- src/force.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <math.h>
-#include "force.h"
-
-double force_calculate_magnitude(double x1, double y1, double x2, double y2) {
-	   return sqrt(pow(x1 - x2,2.0) + pow(y1 - y2,2.0));
-}
--- src/force.h
+++
@@ -1,5 +0,0 @@
-#ifndef FORCE_H
-#define FORCE_H
-double force_calculate_magnitude(double x1, double y1, double x2, double y2);
-
-#endif /* FORCE_H */
